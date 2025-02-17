a8e7cccb46ce83c5293e78e56c649a9657c9c581
somikaaaA <s0mik.a@yandex.ru>
2025-02-17 17:26:26 +0300

    добавлена обработка ошибок (ErrorHandler)

---
+++ main-service/src/main/java/ru/practicum/error/ErrorHandler.java
@@ -0,0 +1,128 @@
+package ru.practicum.error;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.dto.ApiError;
+import ru.practicum.exception.DataBadRequestException;
+import ru.practicum.exception.DataConflictException;
+import ru.practicum.exception.DataNotFoundException;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.LocalDateTime;
+import java.util.Collections;
+
+@RestControllerAdvice
+@Slf4j
+public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        StringWriter out = new StringWriter();
+        e.printStackTrace(new PrintWriter(out));
+        String stackTrace = out.toString();
+        return new ApiError(
+                Collections.singletonList(stackTrace),
+                "Ошибка валидации параметра",
+                e.getMessage(),
+                HttpStatus.BAD_REQUEST.toString(),
+                LocalDateTime.now().toString()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ApiError handleDataIntegrityViolationException(DataIntegrityViolationException e) {
+        StringWriter out = new StringWriter();
+        e.printStackTrace(new PrintWriter(out));
+        String stackTrace = out.toString();
+        return new ApiError(
+                Collections.singletonList(stackTrace),
+                e.getMessage(),
+                "Нарушение целостности данных",
+                HttpStatus.CONFLICT.toString(),
+                LocalDateTime.now().toString()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiError handleDataNotFoundException(DataNotFoundException e) {
+        StringWriter out = new StringWriter();
+        e.printStackTrace(new PrintWriter(out));
+        String stackTrace = out.toString();
+        return new ApiError(
+                Collections.singletonList(stackTrace),
+                e.getMessage(),
+                "Данные не найдены",
+                HttpStatus.NOT_FOUND.toString(),
+                LocalDateTime.now().toString()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ApiError handleDataConflictException(DataConflictException e) {
+        StringWriter out = new StringWriter();
+        e.printStackTrace(new PrintWriter(out));
+        String stackTrace = out.toString();
+        return new ApiError(
+                Collections.singletonList(stackTrace),
+                e.getMessage(),
+                "Данные не удовлетворяют правилам",
+                HttpStatus.CONFLICT.toString(),
+                LocalDateTime.now().toString()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleDataBadRequestException(DataBadRequestException e) {
+        StringWriter out = new StringWriter();
+        e.printStackTrace(new PrintWriter(out));
+        String stackTrace = out.toString();
+        return new ApiError(
+                Collections.singletonList(stackTrace),
+                e.getMessage(),
+                "Запрос составлен некорректно",
+                HttpStatus.BAD_REQUEST.toString(),
+                LocalDateTime.now().toString()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        StringWriter out = new StringWriter();
+        e.printStackTrace(new PrintWriter(out));
+        String stackTrace = out.toString();
+        return new ApiError(
+                Collections.singletonList(stackTrace),
+                e.getMessage(),
+                "Отсутствует обязательный параметр запроса",
+                HttpStatus.BAD_REQUEST.toString(),
+                LocalDateTime.now().toString()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiError handleUnknownException(Throwable e) {
+        StringWriter out = new StringWriter();
+        e.printStackTrace(new PrintWriter(out));
+        String stackTrace = out.toString();
+        return new ApiError(
+                Collections.singletonList(stackTrace),
+                e.getMessage(),
+                "Необрабатываемая ошибка",
+                HttpStatus.INTERNAL_SERVER_ERROR.toString(),
+                LocalDateTime.now().toString()
+        );
+    }
+}
