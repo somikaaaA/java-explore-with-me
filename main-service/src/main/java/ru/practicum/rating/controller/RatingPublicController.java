f91e66aeb087b058164d759aaf46516403194faf
somikaaaA <s0mik.a@yandex.ru>
2025-02-26 18:26:09 +0300

    3 этап

---
+++ main-service/src/main/java/ru/practicum/rating/controller/RatingPublicController.java
@@ -0,0 +1,38 @@
+package ru.practicum.rating.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.rating.dto.EventRatingDto;
+import ru.practicum.rating.dto.UserRatingDto;
+import ru.practicum.rating.model.Rating;
+import ru.practicum.rating.service.RatingService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/rating")
+@AllArgsConstructor
+public class RatingPublicController {
+    private final RatingService ratingService;
+
+    @GetMapping("/events/{eventId}")
+    @ResponseStatus(HttpStatus.OK)
+    public EventRatingDto findByEventId(@PathVariable Long eventId) {
+        return ratingService.findByEventId(eventId);
+    }
+
+    @GetMapping("/users/{userId}")
+    @ResponseStatus(HttpStatus.OK)
+    public UserRatingDto findByUserId(@PathVariable Long userId) {
+        return ratingService.findByUserId(userId);
+    }
+
+    @GetMapping("/events")
+    @ResponseStatus(HttpStatus.OK)
+    public List<EventRatingDto> findEventsWithRating(@RequestParam(defaultValue = "desc") String sort,
+                                                     @RequestParam(defaultValue = "0") Integer from,
+                                                     @RequestParam(defaultValue = "10") Integer size) {
+        return ratingService.findEventsWithRating(sort,from,size);
+    }
+}
---
+++ main-service/src/main/java/ru/practicum/rating/dto/NewRatingDto.java
@@ -0,0 +1,20 @@
+package ru.practicum.rating.dto;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class NewRatingDto {
+    @Min(1)
+    @Max(5)
+    @NotNull
+    private Integer rating;
+}
